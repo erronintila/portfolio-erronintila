199ae059ec2f9b1c091a0fd3cb1ded21a1d8b045
Erron Intila <erronintila@gmail.com>
2021-05-23 03:06:53 +0800

    updated fancybox options

--- resources/script/script.js
+++ resources/script/script.js
@@ -3,6 +3,22 @@ $(document).ready(function () {
     speed: 700,
     speedAsDuration: true,
   });
+
+  $('[data-fancybox="invoice"]').fancybox({
+    scrolling: 'hidden',
+    helpers: {
+      overlay: {
+        locked: false
+      }
+    }
+  });
+
+  $('[data-fancybox="lis"]').fancybox({
+    scrolling: 'hidden',
+    helpers: {
+      overlay: {
+        locked: false
+      }
+    }
+  });
 });
-
-
